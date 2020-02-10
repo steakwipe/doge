f3a3523cc99af410aef929575d2dfac9120046dd
Ray Miller <steakwipe@gmail.com>
2020-02-09 16:19:17 -0800

    made it cate edition

--- doge-tk.py
+++ doge-tk.py
@@ -2,20 +2,20 @@ import tkinter as tk
 from tkinter import messagebox as mb
 
 def in1():
-    mb.showinfo("doge", "the doge liked that :D")
+    mb.showinfo("cate", "the cate liked that :D")
 def in2():
-    mb.showinfo("doge", "the doge is mad D:")
+    mb.showinfo("cate", "the cate is mad D:")
 def in3():
-    mb.showerror("doge", "you killed the doge you monster")
-    mb.showerror("doge", "how could you..")
-    mb.showerror("doge", "don't come near me or my doges")
-    exit("you killed the doge you monster how do you feel about yourself?")
+    mb.showerror("cate", "you killed the cate you monster")
+    mb.showerror("cate", "how could you..")
+    mb.showerror("cate", "don't come near me or my cates")
+    exit("you killed the cate you monster how do you feel about yourself?")
 
-tk.Button(text='pet doge', command=in1).pack(fill=tk.X)
-tk.Button(text='feed doge', command=in1).pack(fill=tk.X)
+tk.Button(text='pet cate', command=in1).pack(fill=tk.X)
+tk.Button(text='feed cate', command=in1).pack(fill=tk.X)
 tk.Button(text='good boy', command=in1).pack(fill=tk.X)
-tk.Button(text='play with he doge', command=in1).pack(fill=tk.X)
-tk.Button(text='take the ball form the doge', command=in2).pack(fill=tk.X)
-tk.Button(text='give the doge the ball', command=in1).pack(fill=tk.X)
-tk.Button(text='kill the doge..', command=in3).pack(fill=tk.X)
+tk.Button(text='play with he cate', command=in1).pack(fill=tk.X)
+tk.Button(text='take the ball form the cate', command=in2).pack(fill=tk.X)
+tk.Button(text='give the cate the ball', command=in1).pack(fill=tk.X)
+tk.Button(text='kill the cate..', command=in3).pack(fill=tk.X)
 tk.mainloop()
